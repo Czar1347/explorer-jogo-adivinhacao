70a12e1a1f7817bd6c1c7a39e56a8e61856c19ca
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-04-24 18:10:33 -0300

    Aplicando a lógica

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
+const randoNumber = Math.round(Math.random() * 10)
+let Attempts = 1
+console.log(randoNumber)
 function handleClick(event) {
     event.preventDefault()
 
-    const inputNumber = document.querySelector("#inputNumber")
-    console.log(inputNumber.value)
+    let inputNumber = document.querySelector("#inputNumber")
+    // let result = inputNumber.value == randoNumber
+
+    
+
+    if(inputNumber.value == randoNumber){
+        document.querySelector('.screen1').classList.add('hide')
+        document.querySelector('.screen2').classList.remove('hide')
+
+        document
+        .querySelector('.screen2 h2')
+        .innerText = `Você acertou em ${Attempts}`
+    }
+    Attempts++
+    console.log(Attempts)
 }
